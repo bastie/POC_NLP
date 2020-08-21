5e13a975659edabcffafe92d2ba7f42781221f87
Sͬeͥbͭaͭsͤtͬian <sebastian.ritter@example.com>
2020-08-21 11:44:19 +0200

    NLTK preface sample reimplemented in Java

--- jnltk/book/biz/ritter/nltk/book/preface/WhyJava.java
+++ jnltk/book/biz/ritter/nltk/book/preface/WhyJava.java
@@ -7,20 +7,16 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 /**
- * very simple Java version
+ * very simple Java version in 10 LOC
  * @author Sͬeͥbͭaͭsͤtͬian
- * @version 0.1
+ * @version 1.0
  */
 public final class WhyJava {
   
   public static void main(final String... args) throws Throwable {
-    for (String line : Files.readAllLines(Paths.get("file.txt"))) {
-      for (String word : line.split (" ")) {
-        if (word.endsWith("ing")) {
+    for (String line : Files.readAllLines(Paths.get("file.txt"))) 
+      for (String word : line.split (" ")) 
+        if (word.endsWith("ing")) 
           System.out.printf("%s%n", word);
-        }
-      }
-    }
   }
-  
 }
