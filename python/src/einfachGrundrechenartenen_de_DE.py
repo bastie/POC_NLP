1c8982371e41b987d7d5fc185044b527a29d7c0d
Sͬeͥbͭaͭsͤtͬian <sebastian.ritter@example.com>
2020-09-01 12:14:05 +0200

    HanTa tagging

--- python/src/einfachGrundrechenartenen_de_DE.py
+++ python/src/einfachGrundrechenartenen_de_DE.py
@@ -1,3 +1,4 @@
+# install certifi, nltk, HanTa, numpy with pip3
 # import certifi
 # certifi.where()
 ### once in time downloaded all
@@ -6,10 +7,11 @@
 ### ERROR 1123 do this...
 ### /Applications/Python 3.6/Install Certificates.command
 
-import nltk
-import codecs
-import os
-import sys
+import nltk     # for NLP 
+from HanTa import HanoverTagger as hTagger    # for NLP (Lemma, POS Tagger) - need numpy
+import codecs   # for encodings
+import os       # for path
+import sys      # for clear exit program
 
 ALL_GOOD = 0
 
@@ -24,11 +26,16 @@ textFile.close()
 
 # get sentences from nltk for german
 allSentences = nltk.sent_tokenize(text,language='german')
-# iter over the sentences, print the sentences and tokenize the sentence in his word before print words
+# iterate over the sentences, print the sentences and 
+# tokenize the sentence in his word before print words and
+# analyse the word typ
+tagger = hTagger.HanoverTagger("morphmodel_ger.pgz")
 for sentences in allSentences:
     print(sentences)
     sentencesToken = nltk.tokenize.word_tokenize(sentences,language='german')
     print(sentencesToken)
+    tags = tagger.tag_sent(sentencesToken)
+    print (tags)
     
     
 # ALL_GOOD = "Oh holy shit"
