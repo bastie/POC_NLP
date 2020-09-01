430cbd475d4a32e1c9f25e71551a2b06e8e4d200
Sͬeͥbͭaͭsͤtͬian <sebastian.ritter@example.com>
2020-09-01 17:21:50 +0200

    remove Java sources

--- jnltk/book/biz/ritter/nltk/book/preface/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * @author Sͬeͥbͭaͭsͤtͬian
- *
- */
-package biz.ritter.nltk.book.preface;
--- jnltk/src/module-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * @author Sͬeͥbͭaͭsͤtͬian
- *
- */
-module biz.ritter.poc.nltk {
-}
